63eadb0e9f339937c12a4e54acbaeab211321415
SpeziFisch-DE <67187041+SpeziFisch-DE@users.noreply.github.com>
2021-02-07 17:08:29 +0100

    serverURL

--- HFU_Twitter/script.js
+++ HFU_Twitter/script.js
@@ -21,8 +21,8 @@ var HFUTwitter;
             window.open("index.html", "_self");
         }
     }
-    //let serverURL: string = "https://hfu-twitter.herokuapp.com";
-    let serverURL = "http://localhost:8100/";
+    let serverURL = "https://hfu-twitter.herokuapp.com";
+    //let serverURL: string = "http://localhost:8100";
     //Sign In Page
     if (getSubpage() == "signin.html") {
         let buttonSignin = document.getElementById("submit");
